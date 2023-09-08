73ac8a29eea3dd0c5bece534206c15ecfd5bc2cb
Adrian Tache <contact@adriantache.com>
2023-09-08 21:57:08 +0200

    Update dependencies

--- app/src/main/java/com/adriantache/gptassistant/presentation/MainActivity.kt
+++ app/src/main/java/com/adriantache/gptassistant/presentation/MainActivity.kt
@@ -16,11 +16,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,7 +27,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
@@ -55,12 +53,12 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
+        tts.stop()
         tts.destroy()
 
         super.onDestroy()
     }
 
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -125,7 +123,7 @@ class MainActivity : ComponentActivity() {
                                 Box(
                                     modifier = Modifier
                                         .fillMaxWidth()
-                                        .background(MaterialTheme.colorScheme.primary, TextFieldDefaults.outlinedShape)
+                                        .background(MaterialTheme.colorScheme.primary, OutlinedTextFieldDefaults.shape)
                                         .padding(8.dp),
                                 ) {
                                     Text(
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 buildscript {
-    extra.apply {
-        set("compose_version","1.3.3")
-    }
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.7.0"))
+        classpath(kotlin("gradle-plugin", version = "1.9.10"))
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
-    kotlin("jvm") version "1.7.0" apply false
+    id("com.android.application") version "8.2.0-beta03" apply false
+    id("com.android.library") version "8.2.0-beta03" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    kotlin("jvm") version "1.9.10" apply false
 }
