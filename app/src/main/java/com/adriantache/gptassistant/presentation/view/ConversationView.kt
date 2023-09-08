44d7f3b67f764fcf653b61535b73f170bc30773e
Adrian Tache <contact@adriantache.com>
2023-09-08 23:47:39 +0200

    Add copy to clipboard functionality

--- app/src/main/java/com/adriantache/gptassistant/presentation/view/ConversationView.kt
+++ app/src/main/java/com/adriantache/gptassistant/presentation/view/ConversationView.kt
@@ -4,14 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
+import com.adriantache.gptassistant.R
 import com.adriantache.gptassistant.data.model.ChatMessage
 import com.adriantache.gptassistant.data.model.ChatRole
 
@@ -19,9 +28,12 @@ import com.adriantache.gptassistant.data.model.ChatRole
 fun ConversationView(
     chatMessage: ChatMessage,
 ) {
-    val bgColor = if (chatMessage.role == ChatRole.user) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
-    val textColor = if (chatMessage.role == ChatRole.user) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
-    val contentAlignment = if (chatMessage.role == ChatRole.user) Arrangement.End else Arrangement.Start
+    val isUserMessage = chatMessage.role == ChatRole.user
+    val bgColor = if (isUserMessage) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
+    val textColor = if (isUserMessage) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
+    val contentAlignment = if (isUserMessage) Arrangement.End else Arrangement.Start
+
+    val clipboardManager = LocalClipboardManager.current
 
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -39,6 +51,19 @@ fun ConversationView(
                 style = MaterialTheme.typography.bodyMedium,
                 color = textColor,
             )
+        }
+
+        if (!isUserMessage) {
+            Spacer(Modifier.width(8.dp))
+
+            IconButton(
+                modifier = Modifier.requiredSize(48.dp),
+                onClick = {
+                    clipboardManager.setText(AnnotatedString(chatMessage.content))
+                }
+            ) {
+                Icon(painter = painterResource(id = R.drawable.baseline_content_copy_24), contentDescription = "Copy message")
+            }
         }
     }
 }
