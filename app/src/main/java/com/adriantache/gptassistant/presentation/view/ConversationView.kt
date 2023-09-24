b349529daf5647541b77054ee6ae2b1cfee83318
Adrian Tache <contact@adriantache.com>
2023-09-24 11:59:00 +0200

    Improve reset conversation button

--- app/src/main/java/com/adriantache/gptassistant/presentation/view/ConversationView.kt
+++ app/src/main/java/com/adriantache/gptassistant/presentation/view/ConversationView.kt
@@ -1,29 +1,42 @@
 package com.adriantache.gptassistant.presentation.view
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.adriantache.gptassistant.R
 import com.adriantache.gptassistant.domain.model.Message
+import com.adriantache.gptassistant.presentation.util.ListScrollEntries
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -39,10 +52,18 @@ fun ConversationView(
     onResetConversation: () -> Unit,
     onLoadPreviousConversations: () -> Unit,
 ) {
-    val scrollState = rememberLazyListState()
+    val lazyListState = rememberLazyListState()
+
+    val entries = remember { ListScrollEntries() }
+    val isFabVisible by remember {
+        derivedStateOf {
+            entries.addItem(lazyListState.firstVisibleItemIndex)
+            !entries.isScrollingDown
+        }
+    }
 
     LaunchedEffect(conversation) {
-        scrollState.animateScrollToItem((conversation.size - 2).coerceAtLeast(0))
+        lazyListState.animateScrollToItem((conversation.size - 2).coerceAtLeast(0))
     }
 
     Column(
@@ -66,7 +87,7 @@ fun ConversationView(
                 .fillMaxWidth()
                 .weight(1f),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            state = scrollState,
+            state = lazyListState,
         ) {
             if (conversation.isEmpty()) {
                 item {
@@ -102,12 +123,29 @@ fun ConversationView(
         if (canResetConversation) {
             Spacer(Modifier.height(16.dp))
 
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                onClick = onResetConversation
-            ) {
-                Text("Reset conversation")
+            AnimatedVisibility(visible = isFabVisible) {
+                FloatingActionButton(
+                    onClick = onResetConversation,
+                    elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_delete_forever_24),
+                            contentDescription = "Reset conversation",
+                            tint = LocalContentColor.current
+                        )
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text("Reset conversation")
+
+                        Spacer(modifier = Modifier.width(8.dp))
+                    }
+                }
             }
         }
     }
