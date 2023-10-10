fcf431d9b4b9826c22728b5e333ce3c7dcbcae0a
Adrian Tache <contact@adriantache.com>
2023-10-10 20:22:54 +0200

    Update gpt model

--- app/src/main/java/com/adriantache/gptassistant/data/model/OpenAiRequest.kt
+++ app/src/main/java/com/adriantache/gptassistant/data/model/OpenAiRequest.kt
@@ -3,8 +3,8 @@ package com.adriantache.gptassistant.data.model
 import kotlinx.serialization.Serializable
 
 @Suppress("unused")
-private const val MODEL_GPT_3 = "gpt-3.5-turbo"
-private const val MODEL_GPT_4 = "gpt-4"
+private const val MODEL_GPT_3 = "gpt-3.5-turbo-0613"
+private const val MODEL_GPT_4 = "gpt-4-0613"
 
 @Serializable
 data class OpenAiRequest(
