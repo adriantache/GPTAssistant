05643e8f391555e38eb26037aec0493799116d34
Adrian Tache <contact@adriantache.com>
2023-10-12 20:36:08 +0200

    Move API key to somewhere safer

--- app/src/main/java/com/adriantache/gptassistant/data/retrofit/OpenAiApi.kt
+++ app/src/main/java/com/adriantache/gptassistant/data/retrofit/OpenAiApi.kt
@@ -1,5 +1,6 @@
 package com.adriantache.gptassistant.data.retrofit
 
+import com.adriantache.gptassistant.BuildConfig
 import com.adriantache.gptassistant.data.model.OpenAiRequest
 import com.adriantache.gptassistant.data.model.OpenAiResponse
 import retrofit2.Response
@@ -7,7 +8,7 @@ import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-private const val API_KEY = [API KEY HERE!!!]
+private const val API_KEY = BuildConfig.OPENAI_SDK_KEY
 
 fun interface OpenAiApi {
     @Headers("Content-Type: application/json", "Authorization: Bearer $API_KEY")
