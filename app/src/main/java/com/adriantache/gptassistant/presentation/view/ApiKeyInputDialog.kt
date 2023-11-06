9beb63f5d3d4f20e4e62e96468baeb6a5f8f9099
Adrian Tache <contact@adriantache.com>
2023-11-06 21:18:13 +0100

    Remove unnecessary lazy list

--- app/src/main/java/com/adriantache/gptassistant/presentation/view/ApiKeyInputDialog.kt
+++ app/src/main/java/com/adriantache/gptassistant/presentation/view/ApiKeyInputDialog.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -47,7 +47,7 @@ fun ApiKeyInputDialog(
         onDismissRequest = { /*ignored since it's a mandatory input*/ },
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
@@ -55,71 +55,61 @@ fun ApiKeyInputDialog(
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            item {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Please enter your OpenAI API key to connect to ChatGPT:",
-                    style = MaterialTheme.typography.headlineSmall,
-                )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Please enter your OpenAI API key to connect to ChatGPT:",
+                style = MaterialTheme.typography.headlineSmall,
+            )
 
-                Spacer(Modifier.height(16.dp))
-            }
+            Spacer(Modifier.height(16.dp))
 
-            item {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Please note that you will need to set up billing in your OpenAI account to access the ChatGPT API.",
-                    style = MaterialTheme.typography.labelSmall,
-                    fontStyle = FontStyle.Italic,
-                )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Please note that you will need to set up billing in your OpenAI account to access the ChatGPT API.",
+                style = MaterialTheme.typography.labelSmall,
+                fontStyle = FontStyle.Italic,
+            )
 
-                Spacer(Modifier.height(8.dp))
-            }
+            Spacer(Modifier.height(8.dp))
 
-            item {
-                TextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = input,
-                    enabled = true,
-                    onValueChange = { input = it },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                    keyboardActions = KeyboardActions(onDone = { onSubmit(input) }),
-                )
-            }
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = input,
+                enabled = true,
+                onValueChange = { input = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = { onSubmit(input) }),
+            )
 
-            item {
-                Spacer(Modifier.height(8.dp))
+            Spacer(Modifier.height(8.dp))
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            uriHandler.openUri(OPENAI_API_KEY_PAGE)
-                        }
-                        .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(4.dp))
-                        .padding(8.dp),
-                ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "Click here to open the OpenAI website to get a key.",
-                        style = MaterialTheme.typography.labelSmall,
-                        textAlign = TextAlign.Center,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                    )
-                }
-
-                Spacer(Modifier.height(8.dp))
-            }
-
-            item {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    Button(
-                        onClick = { onSubmit(input) }) {
-                        Text("Submit")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        uriHandler.openUri(OPENAI_API_KEY_PAGE)
                     }
+                    .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(4.dp))
+                    .padding(8.dp),
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Click here to open the OpenAI website to get a key.",
+                    style = MaterialTheme.typography.labelSmall,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                )
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(
+                    onClick = { onSubmit(input) }) {
+                    Text("Submit")
                 }
             }
         }
