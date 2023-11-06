7d2cb07867e007327205d3ada82165949b1d81c4
Adrian Tache <contact@adriantache.com>
2023-11-06 21:26:26 +0100

    Integrate latest chatgpt model

--- app/src/main/java/com/adriantache/gptassistant/data/model/OpenAiRequest.kt
+++ app/src/main/java/com/adriantache/gptassistant/data/model/OpenAiRequest.kt
@@ -2,12 +2,12 @@ package com.adriantache.gptassistant.data.model
 
 import kotlinx.serialization.Serializable
 
-@Suppress("unused")
-private const val MODEL_GPT_3 = "gpt-3.5-turbo-0613"
+private const val MODEL_GPT_3 = "gpt-3.5-turbo-1106"
 private const val MODEL_GPT_4 = "gpt-4-0613"
+private const val MODEL_GPT_4_TURBO = "gpt-4-1106-preview"
 
 @Serializable
 data class OpenAiRequest(
     val messages: List<ChatMessage>,
-    val model: String = MODEL_GPT_4,
+    val model: String = MODEL_GPT_4_TURBO,
 )
