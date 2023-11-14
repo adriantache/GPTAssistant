456a3d9332b9cd8deb5eefe537e2c4abe4c48d73
Adrian Tache <contact@adriantache.com>
2023-11-14 19:48:28 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 buildscript {
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.9.10"))
+        classpath(kotlin("gradle-plugin", version = "1.9.20"))
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0-rc02" apply false
     id("com.android.library") version "8.2.0-rc02" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    kotlin("jvm") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    kotlin("jvm") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version ("1.9.10") apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version ("1.9.20") apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
