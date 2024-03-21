2bc8507ca765818d4422219643e7c3d4a1652fad
Adrian Tache <contact@adriantache.com>
2024-03-21 12:50:24 +0100

    Fix stability.ai image generation

--- app/src/main/java/com/adriantache/gptassistant/presentation/view/ImageGenerationView.kt
+++ app/src/main/java/com/adriantache/gptassistant/presentation/view/ImageGenerationView.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -83,9 +82,7 @@ fun ImageGenerationView(
                     )
                 } else {
                     val imageBytes = remember(image) {
-                        derivedStateOf {
-                            Base64.decode(image.base64!!)
-                        }
+                        Base64.decode(image.base64!!)
                     }
 
                     AsyncImage(
