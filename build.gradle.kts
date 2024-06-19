a196b7969598549c343203818b56fbfe65e21cb4
Adrian Tache <contact@adriantache.com>
2024-06-19 09:02:53 +0200

    Update to kotlin 2.0 and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val kotlinVersion = "1.9.24"
-    val kspVersion = "$kotlinVersion-1.0.20"
+    val kotlinVersion = "2.0.0"
+    val kspVersion = "$kotlinVersion-1.0.22"
     val gradlePlugin = "8.4.0"
 
     id("com.android.application") version gradlePlugin apply false
@@ -16,6 +16,7 @@ plugins {
     id("com.google.devtools.ksp") version kspVersion apply false
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.1" apply false
+    kotlin("plugin.compose") version kotlinVersion apply false
 }
